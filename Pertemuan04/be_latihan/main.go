d2c4049980c4ff4a09fc388fdb87ffe94c9f689f
nawal886 <nawalharomaen@gmail.com~>
2026-04-20 05:08:48 +0700

    pertemuan 05

--- Pertemuan04/be_latihan/main.go
+++ Pertemuan04/be_latihan/main.go
@@ -3,16 +3,19 @@ package main
 import (
 	"be_latihan/config"
 	"be_latihan/model"
+	"be_latihan/router"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func main() {
 	app := fiber.New()
+	app.Use(logger.New())
 
 	config.InitDB()
-	// AutoMigrate membuat tabel berdasarkan Struct secara otomatis
 	config.GetDB().AutoMigrate(&model.Mahasiswa{})
+	router.SetupRoutes(app)
 
 	app.Listen(":3000")
 }
---
+++ Pertemuan04/be_latihan/router/router.go
@@ -0,0 +1,21 @@
+package router
+
+import (
+	"be_latihan/handler"
+	"be_latihan/model"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func SetupRoutes(app *fiber.App) {
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(model.Response{
+			Message: "API be_latihan aktif",
+		})
+	})
+
+	mahasiswa := app.Group("/api/mahasiswa")
+	mahasiswa.Get("/", handler.GetAllMahasiswa)
+
+	
+}
