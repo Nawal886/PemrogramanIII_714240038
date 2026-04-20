290106330691ccf52fe6e629305729a2fdf8eb0f
nawal886 <nawalharomaen@gmail.com~>
2026-04-20 15:54:26 +0700

    nambah 6

--- Pertemuan05/be_latihan/main.go
+++ Pertemuan05/be_latihan/main.go
@@ -4,8 +4,10 @@ import (
 	"be_latihan/config"
 	"be_latihan/model"
 	"be_latihan/router"
+	"strings"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
@@ -13,6 +15,12 @@ func main() {
 	app := fiber.New()
 	app.Use(logger.New())
 
+	// Basic CORS
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: strings.Join(config.GetAllowedOrigins(), ","),
+		AllowMethods: "GET,POST,PUT,DELETE,OPTIONS",
+	}))
+	
 	config.InitDB()
 	config.GetDB().AutoMigrate(&model.Mahasiswa{})
 	router.SetupRoutes(app)
