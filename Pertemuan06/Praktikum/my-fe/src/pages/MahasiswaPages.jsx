9045dddabe318f698c542881cb7635cf44a30cee
nawal886 <nawalharomaen@gmail.com~>
2026-04-27 04:08:54 +0700

    tes

--- Pertemuan06/Praktikum/my-fe/src/pages/MahasiswaPages.jsx
+++ Pertemuan06/Praktikum/my-fe/src/pages/MahasiswaPages.jsx
@@ -5,6 +5,7 @@ export default function Mahasiswa() {
   const [mahasiswa, setMahasiswa] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
+  const [search, setSearch] = useState("");
 
   const fetchData = () => {
     setLoading(true);
@@ -24,10 +25,42 @@ export default function Mahasiswa() {
 
   if (error) return <p className="text-center text-red-500">Error: {error}</p>;
 
+  const filteredMahasiswa = mahasiswa.filter((mhs) => {
+    const keyword = search.toLowerCase();
+  
+    return (
+      (mhs.nama || "").toLowerCase().includes(keyword) ||
+      (mhs.prodi || "").toLowerCase().includes(keyword) ||
+      (mhs.email || "").toLowerCase().includes(keyword) ||
+      (mhs.alamat || "").toLowerCase().includes(keyword) ||
+      (mhs.npm || "").toString().includes(keyword)
+    );
+  });
+  
   return (
     <div className="max-w-8xl mx-auto p-6">
       <h2 className="text-xl font-bold mb-4">Daftar Mahasiswa</h2>
 
+      <div className="flex justify-between items-center mb-4">
+  <input
+    type="text"
+    placeholder="Cari mahasiswa..."
+    value={search}
+    onChange={(e) => setSearch(e.target.value)}
+    className="border px-4 py-2 rounded w-1/3"
+  />
+
+  <button
+    onClick={fetchData}
+    className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+  >
+    Refresh
+  </button>
+</div>
+
+<p className="mb-2 text-sm text-gray-600">
+  Total Mahasiswa: {filteredMahasiswa.length}
+</p>
       {/* TOTAL */}
     <p className="mb-4 text-gray-600">
       Total Mahasiswa: <span className="font-semibold">{mahasiswa.length}</span>
@@ -46,7 +79,7 @@ export default function Mahasiswa() {
             </tr>
           </thead>
           <tbody className="divide-y">
-            {mahasiswa.map((mhs, index) => (
+          {filteredMahasiswa.map((mhs, index) => (
               <tr key={mhs.npm} className="hover:bg-blue-50">
                 <td className="px-4 py-3 border">{index + 1}</td>
                 <td className="px-4 py-3 text-gray-500 border">{mhs.npm}</td>
