24de4e5e1a98a11e81432cb68253b7023557bf6e
nawal886 <nawalharomaen@gmail.com~>
2026-06-21 17:24:56 +0700

    yee

---
+++ Pertemuan13/Praktikum/be_latihan/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"be_latihan/config"
+	"be_latihan/model"
+	"be_latihan/router"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+func main() {
+	app := fiber.New()
+	app.Use(logger.New())
+
+	// Basic CORS
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: strings.Join(config.GetAllowedOrigins(), ","),
+		AllowMethods: "GET,POST,PUT,DELETE,OPTIONS",
+		AllowHeaders: "Origin,Content-Type,Accept,Authorization",
+	}))
+	
+	config.InitDB()
+	config.GetDB().AutoMigrate(&model.Mahasiswa{}, &model.User{})
+	router.SetupRoutes(app)
+
+	app.Listen(":3000")
+}
