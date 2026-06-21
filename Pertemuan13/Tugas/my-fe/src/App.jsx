0352679164e74149e1ab3218793083decc4f7450
nawal886 <nawalharomaen@gmail.com~>
2026-06-21 17:50:25 +0700

    yaa

---
+++ Pertemuan13/Tugas/my-fe/src/App.jsx
@@ -0,0 +1,38 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import AppLayout from "./components/layout/AppLayout";
+import ChangePasswordPage from "./pages/ChangePasswordPage";
+import DashboardPage from "./pages/DashboardPage";
+import LoginPage from "./pages/LoginPage";
+import MahasiswaDetailPage from "./pages/MahasiswaDetailPage";
+import MahasiswaFormPage from "./pages/MahasiswaFormPage";
+import MahasiswaListPage from "./pages/MahasiswaListPage";
+import ProfilePage from "./pages/ProfilePage";
+import RegisterPage from "./pages/RegisterPage";
+import PrivateRoute from "./routes/PrivateRoute";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/dashboard" replace />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+
+      <Route element={<PrivateRoute />}>
+        <Route element={<AppLayout />}>
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/mahasiswa" element={<MahasiswaListPage />} />
+          <Route path="/mahasiswa/new" element={<MahasiswaFormPage mode="create" />} />
+          <Route path="/mahasiswa/:npm" element={<MahasiswaDetailPage />} />
+          <Route
+            path="/mahasiswa/:npm/edit"
+            element={<MahasiswaFormPage mode="edit" />}
+          />
+          <Route path="/profil" element={<ProfilePage />} />
+          <Route path="/password" element={<ChangePasswordPage />} />
+        </Route>
+      </Route>
+
+      <Route path="*" element={<p>Halaman tidak ditemukan</p>} />
+    </Routes>
+  );
+}
